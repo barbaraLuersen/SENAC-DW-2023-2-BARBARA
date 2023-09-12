5ed121b8cac85076d990a439ff4375834d9e3890
barbaraLuersen <barbaraluersenlima@gmail.com>
2023-09-11 22:04:41 -0300

    Ajustes no html, js e css para executar a busca por resultados filtrados

--- produtos/produtos.js
+++ produtos/produtos.js
@@ -1,23 +1,9 @@
-async function buscarTodosProdutos() {
-  fetch("http://localhost:8080/api/produtos")
-    .then((resultado) => resultado.json())
-    .then((json) => {
-      preencherTabela(json);
-    });
-}
-
-async function listarComSeletor() {
-  fetch("http://localhost:8080/api/produtos/filtros")
-    .then((resultado) => resultado.json())
-    .then((json) => {
-      preencherTabela(json);
-    });
-}
-
 function preencherTabela(jsonProdutos) {
   var dadosTabelaProdutos = document.getElementById("corpoTabela");
 
-  for (let i = 0; i < jsonProdutos.length; i++) {
+  dadosTabelaProdutos.innerHTML = "";
+
+  jsonProdutos.forEach((item) => {
     let novaLinha = dadosTabelaProdutos.insertRow();
 
     let celulaId = novaLinha.insertCell();
@@ -34,8 +20,48 @@ function preencherTabela(jsonProdutos) {
 
     let celulaPeso = novaLinha.insertCell();
     celulaPeso.innerText = jsonProdutos[i].peso;
-  }
+  });
 }
 
-buscarTodosProdutos();
-listarComSeletor();
+window.addEventListener("DOMContentLoaded", buscarProdutoSeletor());
+
+async function buscarProdutoSeletor() {
+  fetch("http://localhost:8080/api/filtros", {
+    method: "POST",
+    body: JSON.stringify({
+      nome: document.getElementById("input-produto").value,
+      fabricante: document.getElementById("input-fabricante").value,
+      cnpjFabricante: document.getElementById("input-cnpj").value,
+      valorMinimo: document.getElementById("input-valor-min").value,
+      valorMaximo: document.getElementById("input-valor-max").value,
+      PesoMinimo: document.getElementById("input-peso-min").value,
+      pesoMaximo: document.getElementById("input-peso-max").value,
+      dataCadastroInicial: document.getElementById("input-data-inicio").value,
+      dataCadastroFinal: document.getElementById("input-data-fim").value,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((resultado) => resultado.json())
+    .then((json) => {
+      console.log(json);
+      preencherTabela(json);
+    });
+}
+
+function esconderFiltro() {
+  document.getElementById("meuConteudo").classList.toggle("show");
+}
+
+window.onclick = (event) => {
+  if (!event.target.matches(".btn-drop")) {
+    var dropdown = document.getElementsByClassName("dropdown-conteudo");
+    dropdowns.forEach((item) => {
+      var openDropdown = dropdown[item];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    });
+  }
+};

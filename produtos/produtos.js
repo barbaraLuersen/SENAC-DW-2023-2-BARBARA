b59ac0451260dabc685202334858cc37c312028e
barbaraLuersen <barbaraluersenlima@gmail.com>
2023-09-18 17:57:14 -0300

    Criação de uma parte para salvar, atualizar e deletar no html. Criação dos métodos: GET: - listarTodos() -consultarPorId()
    POST:
    -salvar()
    -buscarProdutoSeletor()
    
    PUT:
    -atualizar
    
    DELETE:
    -excluir()

--- produtos/produtos.js
+++ produtos/produtos.js
@@ -27,6 +27,62 @@ function preencherTabela(jsonProdutos) {
 
 window.addEventListener("DOMContentLoaded", buscarProdutoSeletor());
 
+//MÉTODOS GET
+async function listarTodos() {
+  fetch("http://localhost:8080/api/produtos/todos", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((resultado) => resultado.json())
+    .then((json) => {
+      console.log(json);
+      preencherTabela(json);
+    });
+}
+
+async function consultarPorId() {
+  fetch(
+    "http://localhost:8080/api/produtos/{" +
+      document.getElementById("input-ID").value +
+      "}",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    }
+  )
+    .then((resultado) => resultado.json())
+    .then((json) => {
+      console.log(json);
+      preencherTabela(json);
+    });
+}
+
+//MÉTODOS POST
+async function salvar() {
+  fetch("http://localhost:8080/api/produtos", {
+    method: "POST",
+    body: JSON.stringify({
+      nome: document.getElementById("input-produto").value,
+      fabricante: document.getElementById("input-fabricante").value,
+      cnpjFabricante: document.getElementById("input-cnpj").value,
+      valorMinimo: document.getElementById("input-valor").value,
+      PesoMinimo: document.getElementById("input-peso").value,
+      dataCadastroInicial: document.getElementById("input-data").value,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((resultado) => resultado.json())
+    .then((json) => {
+      console.log(json);
+    });
+}
+
 async function buscarProdutoSeletor() {
   fetch("http://localhost:8080/api/produtos/filtro", {
     method: "POST",
@@ -52,15 +108,56 @@ async function buscarProdutoSeletor() {
     });
 }
 
-window.onclick = (event) => {
-  if(!event.target.matches('.btn-drop')) {
-    var dropdowns = document.getElementsByClassName('dropdown-conteudo');
+//MÉTODOS PUT
+async function atualizar() {
+  fetch("http://localhost:8080/api/produtos", {
+    method: "PUT",
+    body: JSON.stringify({
+      nome: document.getElementById("input-produto").value,
+      fabricante: document.getElementById("input-fabricante").value,
+      cnpjFabricante: document.getElementById("input-cnpj").value,
+      valorMinimo: document.getElementById("input-valor").value,
+      PesoMinimo: document.getElementById("input-peso").value,
+      dataCadastroInicial: document.getElementById("input-data").value,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((resultado) => resultado.json())
+    .then((json) => {
+      console.log(json);
+    });
+}
 
-    for(let i; i < dropdowns.length; i++){
+//MÉTODO DELETE
+async function excluir() {
+  fetch(
+    "http://localhost:8080/api/produtos/filtro/{" +
+      document.getElementById("input-ID").value +
+      "}",
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    }
+  )
+    .then((resultado) => resultado.json())
+    .then((json) => {
+      console.log(json);
+    });
+}
+
+window.onclick = (event) => {
+  if (!event.target.matches(".btn-drop")) {
+    var dropdowns = document.getElementsByClassName("dropdown-conteudo");
+
+    for (let i; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[item];
-      if(openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      };
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
     }
   }
-}
+};

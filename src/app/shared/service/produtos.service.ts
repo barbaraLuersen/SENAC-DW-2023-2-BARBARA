ad2e575c772c34db02c62099f8a132ba9c5c9db7
Bárbara Luersen <barbaraluersenlima@gmail.com>
2023-10-03 11:18:26 -0300

    O que foi feito: - Criação "shared" - Criação "model" - Criação "seletor" - Criação produto.seletor.ts e fabricante.seletor.ts (entity) - Criação produto.ts e fabricante.ts (entity) - Ajuste em tsconfig.json - No terminal cmd digitar: ng generate service shared/service/produtos - Criação "service" - Criação fabricante.service.ts e produto.service.ts

---
+++ src/app/shared/service/produtos.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../model/produto';
+import { ProdutoSeletor } from '../model/seletor/produto.seletor';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProdutosService {
+  private readonly API = 'http://localhost:8080/api/produtos';
+  constructor(private httpClient: HttpClient) {}
+
+  //Declarar os métodos da API REST
+  listarTodos(): Observable<Array<Produto>> {
+    //com retorno EXPLICITO(Observable<Array<Produto>>)
+    return this.httpClient.get<Array<Produto>>(this.API);
+  }
+
+  listarComSeletor(seletor: ProdutoSeletor) {
+    //com retorno IMPLICITO
+    return this.httpClient.post<Array<Produto>>(this.API + '/filtro', seletor);
+  }
+
+  salvar(produto: Produto): Observable<Produto> {
+    return this.httpClient.post<Produto>(this.API, produto);
+  }
+}

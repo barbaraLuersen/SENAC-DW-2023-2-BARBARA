39d3bd7044e9758be9f237682bd1eb1ab0c8b70a
barbaraLuersen <barbaraluersenlima@gmail.com>
2023-10-10 09:32:01 -0300

    criação dos atributos em produto.seletor

--- src/app/shared/model/seletor/produto.seletor.ts
+++ src/app/shared/model/seletor/produto.seletor.ts
@@ -1,5 +1,11 @@
-
 export class ProdutoSeletor {
-  //TODO criar
+  nome: string;
+  nomeFabricante: string;
+  valorMinimo: number;
+  valorMaximo: number;
+  pesoMinimo: number;
+  pesoMaximo: number;
+  dataCadastroInicial: Date;
+  dataCadastroFinal: Date;
+  cnpjFabricante: string;
 }
-
